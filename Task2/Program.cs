96d04be3e71e5206cf6e8a30a79591aafd3c6811
Eva <okamenoame@gmail.com>
2023-03-25 01:21:10 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число C: ");
+int C = int.Parse(Console.ReadLine()!);
+int max = A;
+
+if (B>max)
+{ 
+    max = B;
+ 
+}
+if (C>max)
+{ 
+    max = C;
+Console.WriteLine($"{max} максимальное из трех значений");
+}
+else
+{
+    max = B;
+Console.WriteLine($"{max} максимальное из трех значений"); 
+}
+
