4f57ad40db491111df11b8a8f474eb7f7cbfcc65
Eva <okamenoame@gmail.com>
2023-03-25 01:18:36 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+if (A>B)
+{ 
+    Console.WriteLine($"{A} наибольшее значение");
+}
+else
+{
+   Console.WriteLine($"{B} наибольшее значение"); 
+}
