0cb250aef8e8d6b71fb685189a0a3c848e6d52b8
Eva <okamenoame@gmail.com>
2023-03-25 01:23:47 +0300

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число : ");
+int A = int.Parse(Console.ReadLine());
+int N = 1;
+
+
+for (int i = N; i <= A; i++ )
+            {
+              if (i % 2 == 0)
+                 {
+                Console.WriteLine(i);
+                  }
+
+       }
