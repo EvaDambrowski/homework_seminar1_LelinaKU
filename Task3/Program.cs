a32a1d3ae3de95ab3c6c429f40f5859ad0187295
Eva <okamenoame@gmail.com>
2023-03-25 01:22:19 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+if (A % 2 ==0)
+{
+ 
+    Console.WriteLine($"{A} четное число");
+}
+else
+{
+    Console.WriteLine($"{A} нечетное число");
+}
